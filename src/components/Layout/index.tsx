3a9f8170b77377a565d62ca362d6a92085d5538b
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-06-11 17:43:00 -0300

    feat: create Layout component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,43 @@
-import React from "react";
-
-// import { Container } from './styles';
+import { ChangeEvent, useRef, useState } from "react";
+import { FiPlay, FiSend } from "react-icons/fi";
+import Message from "../Message";
 
 const Layout = () => {
-  return <div className="layout">bac</div>;
+  const [inputHeight, setInputHeight] = useState("auto");
+  const inputRef = useRef<HTMLTextAreaElement>(null);
+  const handleInputChange = (event: ChangeEvent) => {
+    if (inputRef.current) {
+      inputRef.current.style.height = "auto";
+      inputRef.current.style.height = `${event.target.scrollHeight}px`;
+    }
+  };
+  return (
+    <div className="layout">
+      <div className="layout__chat">
+        <Message
+          message="Message"
+          name="Marllon"
+          time="22:04"
+        />
+      </div>
+      <footer className="layout__bottom">
+        <form className="layout__bottom-form">
+          <textarea
+            className="layout__bottom-input"
+            name="message"
+            autoFocus
+            placeholder="Mensagem"
+            rows={1}
+            ref={inputRef}
+            onChange={handleInputChange}
+          />
+          <button className="layout__bottom-submit">
+            <FiSend size={24} />
+          </button>
+        </form>
+      </footer>
+    </div>
+  );
 };
 
 export { Layout };
