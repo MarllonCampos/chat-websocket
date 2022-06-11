34b2209f0356fe1cf393260df3965a5a67baec14
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-06-11 13:16:19 -0300

    feat: saving info on localStorage, redirecting user to new page

--- src/components/Register/index.tsx
+++ src/components/Register/index.tsx
@@ -1,14 +1,62 @@
 import { FormEvent, useRef } from "react";
+import { useNavigate, useRoutes } from "react-router-dom";
+
+const HOURSTOMINUTES = 60;
+const MINUTESTOSECONDS = 60;
+const SECONDSTOMILISECONDS = 1000;
+const DAYSTOHOURS = 24;
 
 export const Register = () => {
   const inputName = useRef<HTMLInputElement>(null);
   const inputGroupId = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
+
   const formSubmit = (event: FormEvent) => {
     event.preventDefault();
     if (!inputName.current || !inputGroupId.current) return;
     const { value: name } = inputName.current;
     const { value: groupId } = inputGroupId.current;
-    console.log({ name, groupId });
+    const nameValidate = validateEmpty(name);
+    const groupIdValidate = validateEmpty(groupId);
+    if (nameValidate)
+      return console.log("Nome curto demais");
+    if (groupIdValidate)
+      return console.log("Id do grupo preenchido errado");
+
+    saveKeysOnLocalStorage(name, groupId);
+    navigate(`/${groupId}`);
+  };
+  const getKeysFromLocalStorage = (): {
+    name: string;
+    groupId: string;
+  } => {
+    const name =
+      window.localStorage.getItem("chat-websocket-name") ||
+      "";
+    const groupId =
+      window.localStorage.getItem(
+        "chat-websocket-groupId"
+      ) || "";
+
+    return { name, groupId };
+  };
+  const saveKeysOnLocalStorage = (
+    name: string,
+    groupId: string
+  ) => {
+    window.localStorage.setItem(
+      "chat-websocket-name",
+      name
+    );
+    window.localStorage.setItem(
+      "chat-websocket-groupId",
+      groupId
+    );
+  };
+
+  const validateEmpty = (value: string) => {
+    if (value.length < 1) return true;
+    if (!value) return true;
   };
   return (
     <div className="register">
