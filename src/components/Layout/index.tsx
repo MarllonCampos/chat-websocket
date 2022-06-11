58da893bff41f11f670f0cb7df3b5d15305f99ce
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-06-11 19:05:03 -0300

    feat: prevent breakline when textarea is empty

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,5 +1,16 @@
-import { ChangeEvent, useRef, useState } from "react";
-import { FiPlay, FiSend } from "react-icons/fi";
+import {
+  ChangeEvent,
+  FormEvent,
+  KeyboardEventHandler,
+  useRef,
+  useState,
+} from "react";
+import { FiSend } from "react-icons/fi";
+import {
+  getActualGroup,
+  getUserName,
+} from "../../helpers/user";
+import { validateEmpty } from "../../helpers/validateString";
 import Message from "../Message";
 const message =
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis enim eveniet, nemo amet at sed, repellat perspiciatis dolorem quis quasi minus animi asperiores cumque ut aliquid laboriosam similique labore esse.";
@@ -7,11 +18,32 @@ const message =
 const Layout = () => {
   const [inputHeight, setInputHeight] = useState("auto");
   const inputRef = useRef<HTMLTextAreaElement>(null);
+
   const handleInputChange = (event: ChangeEvent) => {
-    if (inputRef.current) {
-      inputRef.current.style.height = "auto";
-      inputRef.current.style.height = `${event.target.scrollHeight}px`;
+    if (!inputRef.current) return;
+    inputRef.current.style.height = "auto";
+    inputRef.current.style.height = `${event.target.scrollHeight}px`;
+  };
+
+  const handleKeyPress = (
+    event: React.KeyboardEvent<HTMLTextAreaElement>
+  ) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      return handleSendMessage();
     }
+  };
+  const handleSendMessage = (event?: FormEvent) => {
+    event?.preventDefault();
+    if (!inputRef.current) return;
+    const { value: message } = inputRef.current;
+    if (validateEmpty(message)) return;
+
+    console.log("send message", {
+      message,
+      owner: getUserName(),
+      groupId: getActualGroup(),
+    });
   };
   return (
     <div className="layout">
@@ -51,6 +83,7 @@ const Layout = () => {
             rows={1}
             ref={inputRef}
             onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
           />
           <button className="layout__bottom-submit">
             <FiSend size={20} />
