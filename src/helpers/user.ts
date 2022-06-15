e05f3274f06b81b41d4487328e92ac4816feb812
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-06-15 02:01:47 -0300

    refactor: moved items to another folder

--- src/helpers/user.ts
+++
@@ -1,8 +0,0 @@
-const getUserName = (): string => {
-  return window.localStorage.getItem("chat-websocket-name") || ""
-}
-
-const getActualGroup = (): string => {
-  return window.localStorage.getItem("chat-websocket-groupId") || ""
-}
-export { getUserName, getActualGroup }
--- src/helpers/validateString.ts
+++
@@ -1,5 +0,0 @@
-const validateEmpty = (value: string) => {
-  if (value.length < 1) return true;
-  if (!value) return true;
-};
-export { validateEmpty }
