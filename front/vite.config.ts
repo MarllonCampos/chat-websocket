570df9488c4d6ecfa0f9d68fcf32f75cde4db3ca
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-02-27 17:06:45 -0300

    feat: change vite configuration

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    port: Number(process.env.PORT),
+    host: "0.0.0.0",
+
+  }
 })
+
