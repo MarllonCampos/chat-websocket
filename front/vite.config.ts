92bf67ae3d7628f67f19088c6a14742b264bfe14
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-02-27 17:22:29 -0300

    ci: change vite to deploy

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import 'dotenv/config'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: Number(process.env.PORT),
-    host: "0.0.0.0",
+export default defineConfig(({ mode }) => {
 
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(process.env.PORT),
+      host: "0.0.0.0",
+    },
+    preview: {
+      port: Number(process.env.PORT),
+      host: "0.0.0.0"
+    }
   }
 })
 
