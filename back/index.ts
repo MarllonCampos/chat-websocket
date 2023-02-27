62be9443dc4f4ab0d00bba4e7bc89962f68bc14e
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-02-27 16:25:50 -0300

    feat: add PORT variable to console log

--- back/index.ts
+++ back/index.ts
@@ -2,5 +2,5 @@ import { serverHttp } from "./src/http";
 import "./src/websocket"
 const PORT = process.env.PORT
 serverHttp.listen(PORT, () => {
-  console.log("Server listen on port 3003")
+  console.log(`Server listen on port ${PORT}`)
 })
